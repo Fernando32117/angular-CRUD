528b0ba7c1581a0ee1746e9f16f97387cfb4bec9
nando32117@gmail.com <nando32117@gmail.com>
2024-01-24 16:38:29 -0300

    varias modificações

---
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-crud',
+  templateUrl: './product-crud.component.html',
+  styleUrl: './product-crud.component.css'
+})
+export class ProductCrudComponent {
+
+}
